20ff17c2f758792d8266fd382c76c9939a3e687c
Tribikram Adhikari <tribikram@sting.graceland.edu>
2019-09-20 09:42:33 -0500

    Initial commit

---
+++ Day1Assignments/src/assignment8/Operator.java
@@ -0,0 +1,16 @@
+package assignment8;
+
+public class Operator {
+	public static void main(String[] args) {
+	
+		int intVal = 100;
+		byte byteVal = (byte) intVal;
+		byte max = 127;
+		byte min = -128;
+		byte sum = (byte) (max + min);
+	
+		System.out.println("Sum= " + sum);
+		
+	
+	}
+}
